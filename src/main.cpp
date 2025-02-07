18ff2d2690594642062a0adc97df33e869fb5b53
Mahmoud Khedr <mahmoud.s.khedr.2@gmail.com>
2025-02-07 22:36:29 +0200

    v .01

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+int main(){
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
+
+    while (true)
+    {
+        std::cout << "$";
+        std::string input;
+        std::getline(std::cin, input);
+
+        if (input == "exit"){
+            return 0;
+        }else{
+            system(input.c_str());
+        }
+
+    }
+
+    return 0;
+}
